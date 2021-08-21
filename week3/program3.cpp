b4a3f8abb6c284bb9d71c953373e960f8ef29e3a
Manjeet Kumar Singh <manjeets1245@gmail.com>
2021-08-21 12:57:24 +0530

    initial comiit

---
+++ week3/program3.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+#define sync ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define check(x)                cerr << #x << ": " << x << endl;
+ll isPrime(ll n) {
+   ll i;
+   for(i=2; i*i<=n; ++i) {
+      if(n%i==0) {
+        
+         
+         return 0;
+      }
+   }
+   return 1;
+  
+}
+
+void solve()
+{
+ int n;
+ cin>>n;
+ int arr[n];
+ for(int i=0;i<n;i++)
+    cin>>arr[i];
+ 
+ sort(arr,arr+n);
+ bool flag=false;
+
+ for(int i=0;i<n-1;i++)
+ {
+    if(arr[i]==arr[i+1])
+    {
+        flag=true;
+        break;
+    }
+ }
+ if(flag)
+    cout<<"YES\n";
+ else
+    cout<<"NO\n";
+
+}
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    sync;
+    int t = 1;
+    cin >> t;
+    while(t--) solve();
+    cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << "\n";
+    return 0;    
+}
